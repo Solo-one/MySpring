e1173fc81db304eef8ed05f2b96771368a59ae3d
fangxiaoming01 <fangxiaoming01@baidu.com>
2018-02-28 19:46:42 +0800

    add PageHelper

--- src/main/java/com/xiaoming/config/DruidDataSourceConfiguration.java
+++ src/main/java/com/xiaoming/config/DruidDataSourceConfiguration.java
@@ -3,11 +3,15 @@ package com.xiaoming.config;
 import javax.sql.DataSource;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
 
 /**
  * @author by fangxiaoming01
@@ -23,4 +27,26 @@ public class DruidDataSourceConfiguration {
         DruidDataSource druidDataSource = new DruidDataSource();
         return druidDataSource;
     }
+
+    @Bean
+    public ServletRegistrationBean druidServlet() {
+        ServletRegistrationBean reg = new ServletRegistrationBean();
+        reg.setServlet(new StatViewServlet());
+        reg.addUrlMappings("/druid/*");
+        reg.addInitParameter("allow", "127.0.0.1"); //白名单
+        //reg.addInitParameter("deny",""); //黑名单
+        reg.addInitParameter("loginUsername", "admin");
+        reg.addInitParameter("loginPassword", "admin");
+        return reg;
+    }
+
+    @Bean
+    public FilterRegistrationBean filterRegistrationBean() {
+        FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean();
+        filterRegistrationBean.setFilter(new WebStatFilter());
+        filterRegistrationBean.addUrlPatterns("/*");
+        filterRegistrationBean.addInitParameter("exclusions", "*.js,*.gif,*.jpg,*.png,*.css,*.ico,/druid/*");
+        return filterRegistrationBean;
+    }
+
 }
--- src/main/java/com/xiaoming/service/impl/UserServiceImpl.java
+++ src/main/java/com/xiaoming/service/impl/UserServiceImpl.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.xiaoming.bean.UserInfo;
 import com.xiaoming.dao.UserMapper;
 import com.xiaoming.service.UserService;
@@ -21,7 +23,12 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public List<UserInfo> queryUsers() {
-        return userDao.queryUsers();
+        // PageHelper 分页
+        PageHelper.startPage(1, 2, true);
+        List<UserInfo> list = userDao.queryUsers();
+        PageInfo pageInfo = new PageInfo(list);
+        // TODO 分页详细信息
+        return list;
     }
 
 }
